1eb0702ce50ec3e754db2c7414163eaa0b0c4586
FatttSnake <fatttsnake@gmail.com>
2025-03-21 01:18:09 +0800

    Refactor(GeneralSetting): Add page scrolling

--- src/renderer/settings/assets/css/components/settings-control/index.style.ts
+++ src/renderer/settings/assets/css/components/settings-control/index.style.ts
@@ -3,6 +3,7 @@ import { createStyles } from 'antd-style'
 export default createStyles(({ token }) => ({
     root: {
         marginTop: token.sizeLG,
+        paddingBottom: token.paddingXL,
         gap: token.sizeXXL
     }
 }))
--- src/renderer/settings/pages/base/General.tsx
+++ src/renderer/settings/pages/base/General.tsx
@@ -1,4 +1,6 @@
 import Icon from '@ant-design/icons'
+import FitFullscreen from '$/components/FitFullscreen'
+import HideScrollbar from '$/components/HideScrollbar'
 import SettingsControl from '%/components/SettingsControl'
 
 const General = () => {
@@ -22,78 +24,96 @@ const General = () => {
     }, [])
 
     return (
-        <SettingsControl header={'常规'}>
-            <SettingsControl.Group title={'外观'}>
-                <SettingsControl.Item icon={IconOxygenLanguage} name={'语言'} desc={'界面语言'}>
-                    <AntdSelect value={'zh_CN'}>
-                        <AntdSelect.Option value={'zh_CN'}>中文</AntdSelect.Option>
-                    </AntdSelect>
-                </SettingsControl.Item>
-                <SettingsControl.Item icon={IconOxygenColor} name={'主题'} desc={'主题颜色'}>
-                    <AntdSegmented<WindowTheme>
-                        options={[
-                            {
-                                icon: <Icon component={IconOxygenThemeSystem} />,
-                                title: '跟随系统',
-                                value: 'FOLLOW_SYSTEM'
-                            },
-                            {
-                                label: <Icon component={IconOxygenThemeLight} />,
-                                title: '亮色',
-                                value: 'LIGHT'
-                            },
-                            {
-                                label: <Icon component={IconOxygenThemeDark} />,
-                                title: '深色',
-                                value: 'DARK'
+        <FitFullscreen>
+            <HideScrollbar isShowVerticalScrollbar autoHideWaitingTime={1000}>
+                <SettingsControl header={'常规'}>
+                    <SettingsControl.Group title={'外观'}>
+                        <SettingsControl.Item
+                            icon={IconOxygenLanguage}
+                            name={'语言'}
+                            desc={'界面语言'}
+                        >
+                            <AntdSelect value={'zh_CN'}>
+                                <AntdSelect.Option value={'zh_CN'}>中文</AntdSelect.Option>
+                            </AntdSelect>
+                        </SettingsControl.Item>
+                        <SettingsControl.Item
+                            icon={IconOxygenColor}
+                            name={'主题'}
+                            desc={'主题颜色'}
+                        >
+                            <AntdSegmented<WindowTheme>
+                                options={[
+                                    {
+                                        icon: <Icon component={IconOxygenThemeSystem} />,
+                                        title: '跟随系统',
+                                        value: 'FOLLOW_SYSTEM'
+                                    },
+                                    {
+                                        label: <Icon component={IconOxygenThemeLight} />,
+                                        title: '亮色',
+                                        value: 'LIGHT'
+                                    },
+                                    {
+                                        label: <Icon component={IconOxygenThemeDark} />,
+                                        title: '深色',
+                                        value: 'DARK'
+                                    }
+                                ]}
+                                value={theme}
+                                onChange={handleOnChangeTheme}
+                                block
+                            />
+                        </SettingsControl.Item>
+                    </SettingsControl.Group>
+                    <SettingsControl.Group title={'关于'}>
+                        <SettingsControl.Item
+                            icon={IconOxygenLogo}
+                            name={'版本'}
+                            desc={'应用版本名称'}
+                        >
+                            {appVersion}
+                        </SettingsControl.Item>
+                        <SettingsControl.Item
+                            icon={IconOxygenCode}
+                            name={'项目'}
+                            desc={'本应用为开源项目'}
+                            onClick={() =>
+                                oxygenApi.app.url.open(
+                                    'https://github.com/FatttSnake/oxygen-desktop'
+                                )
                             }
-                        ]}
-                        value={theme}
-                        onChange={handleOnChangeTheme}
-                        block
-                    />
-                </SettingsControl.Item>
-            </SettingsControl.Group>
-            <SettingsControl.Group title={'关于'}>
-                <SettingsControl.Item icon={IconOxygenLogo} name={'版本'} desc={'应用版本名称'}>
-                    {appVersion}
-                </SettingsControl.Item>
-                <SettingsControl.Item
-                    icon={IconOxygenCode}
-                    name={'项目'}
-                    desc={'本应用为开源项目'}
-                    onClick={() =>
-                        oxygenApi.app.url.open('https://github.com/FatttSnake/oxygen-desktop')
-                    }
-                >
-                    <Icon component={IconOxygenOpen} />
-                </SettingsControl.Item>
-                <SettingsControl.Item
-                    icon={IconOxygenOpenSource}
-                    name={'开源协议'}
-                    desc={'本项目遵循 GPL-3.0 开源协议'}
-                    onClick={() =>
-                        oxygenApi.app.url.open(
-                            'https://github.com/FatttSnake/oxygen-desktop/blob/master/LICENSE'
-                        )
-                    }
-                >
-                    <Icon component={IconOxygenOpen} />
-                </SettingsControl.Item>
-                <SettingsControl.Item
-                    icon={IconOxygenIssue}
-                    name={'工单'}
-                    desc={'反馈遇到的问题或提出建议'}
-                    onClick={() =>
-                        oxygenApi.app.url.open(
-                            'https://github.com/FatttSnake/oxygen-desktop/issues'
-                        )
-                    }
-                >
-                    <Icon component={IconOxygenOpen} />
-                </SettingsControl.Item>
-            </SettingsControl.Group>
-        </SettingsControl>
+                        >
+                            <Icon component={IconOxygenOpen} />
+                        </SettingsControl.Item>
+                        <SettingsControl.Item
+                            icon={IconOxygenOpenSource}
+                            name={'开源协议'}
+                            desc={'本项目遵循 GPL-3.0 开源协议'}
+                            onClick={() =>
+                                oxygenApi.app.url.open(
+                                    'https://github.com/FatttSnake/oxygen-desktop/blob/master/LICENSE'
+                                )
+                            }
+                        >
+                            <Icon component={IconOxygenOpen} />
+                        </SettingsControl.Item>
+                        <SettingsControl.Item
+                            icon={IconOxygenIssue}
+                            name={'工单'}
+                            desc={'反馈遇到的问题或提出建议'}
+                            onClick={() =>
+                                oxygenApi.app.url.open(
+                                    'https://github.com/FatttSnake/oxygen-desktop/issues'
+                                )
+                            }
+                        >
+                            <Icon component={IconOxygenOpen} />
+                        </SettingsControl.Item>
+                    </SettingsControl.Group>
+                </SettingsControl>
+            </HideScrollbar>
+        </FitFullscreen>
     )
 }
 
