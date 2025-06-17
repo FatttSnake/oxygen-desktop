f90267cdf1551338d39df93761febfaf330f967d
FatttSnake <fatttsnake@gmail.com>
2025-06-18 00:06:55 +0800

    refactor(playground): optimize code structure

--- src/renderer/common/components/Playground/CodeEditor/FileSelector/index.tsx
+++ src/renderer/common/components/Playground/CodeEditor/FileSelector/index.tsx
@@ -1,4 +1,4 @@
-import useStyles from '$/components/Playground/CodeEditor/FileSelector/index.style'
+import useStyles from '$/assets/css/components/playground/code-editor/file-selector.style'
 import HideScrollbar, { HideScrollbarElement } from '$/components/HideScrollbar'
 import FlexBox from '$/components/FlexBox'
 import { IFiles } from '$/components/Playground/shared'
