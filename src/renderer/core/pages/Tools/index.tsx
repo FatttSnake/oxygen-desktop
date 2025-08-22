52685c0789f50a1b36d97495cc2dcf55d9419078
FatttSnake <fatttsnake@gmail.com>
2025-08-22 17:10:25 +0800

    refactor(tool-repository): adapt to new logic for delist and relist

--- src/renderer/core/pages/Tools/index.tsx
+++ src/renderer/core/pages/Tools/index.tsx
@@ -1,3 +1,4 @@
+import { cloneElement, ReactElement, ReactNode } from 'react'
 import Icon from '@ant-design/icons'
 import useStyles from '@/assets/css/pages/tools/index.style'
 import {
@@ -31,6 +32,8 @@ import RepositoryCard from '@/components/tools/RepositoryCard'
 import LoadMoreCard from '@/components/tools/LoadMoreCard'
 import StoreCard from '@/components/tools/StoreCard'
 
+const { Text } = AntdTypography
+
 interface ToolCardProps {
     tools: ToolVo[]
     onDelete?: (tool: ToolVo) => void
@@ -56,7 +59,7 @@ const ToolCard = ({ tools, onDelete, onUpgrade, onSubmit, onCancel }: ToolCardPr
     }
 
     const handleOnEditTool = () => {
-        if (['NONE', 'REJECT'].includes(selectedTool.review)) {
+        if (['NONE'].includes(selectedTool.review)) {
             return () => {
                 if (checkDesktop() || selectedTool.platform === 'WEB') {
                     navigateToEdit(navigate, selectedTool.toolId, selectedTool.platform)
@@ -69,7 +72,7 @@ const ToolCard = ({ tools, onDelete, onUpgrade, onSubmit, onCancel }: ToolCardPr
     }
 
     const handleOnSourceTool = () => {
-        if (selectedTool.review === 'PASS') {
+        if (selectedTool.review !== 'NONE') {
             return () => {
                 navigateToSource(
                     navigate,
@@ -84,7 +87,7 @@ const ToolCard = ({ tools, onDelete, onUpgrade, onSubmit, onCancel }: ToolCardPr
     }
 
     const handleOnPublishTool = () => {
-        if (['NONE', 'REJECT'].includes(selectedTool.review)) {
+        if (['NONE'].includes(selectedTool.review)) {
             return () => {
                 onSubmit?.(selectedTool)
             }
@@ -111,7 +114,7 @@ const ToolCard = ({ tools, onDelete, onUpgrade, onSubmit, onCancel }: ToolCardPr
 
     const toolsGroupByPlatform = (tools: ToolVo[]) => {
         interface Node {
-            label: string
+            label: ReactNode
             value: string
             children?: Node[]
         }
@@ -123,7 +126,14 @@ const ToolCard = ({ tools, onDelete, onUpgrade, onSubmit, onCancel }: ToolCardPr
                     value: value.platform,
                     children: [
                         {
-                            label: `${value.ver}${value.review !== 'PASS' ? '*' : ''}`,
+                            label: (
+                                <Text delete={value.review === 'REJECT'}>
+                                    {value.ver}
+                                    {value.review === 'NONE' || value.review === 'PROCESSING'
+                                        ? '*'
+                                        : ''}
+                                </Text>
+                            ),
                             value: value.id
                         }
                     ]
@@ -133,7 +143,14 @@ const ToolCard = ({ tools, onDelete, onUpgrade, onSubmit, onCancel }: ToolCardPr
                     !temp.some((platform, platformIndex) => {
                         if (platform.value === value.platform) {
                             temp[platformIndex].children!.push({
-                                label: `${value.ver}${value.review !== 'PASS' ? '*' : ''}`,
+                                label: (
+                                    <Text delete={value.review === 'REJECT'}>
+                                        {value.ver}
+                                        {value.review === 'NONE' || value.review === 'PROCESSING'
+                                            ? '*'
+                                            : ''}
+                                    </Text>
+                                ),
                                 value: value.id
                             })
                             return true
@@ -146,7 +163,14 @@ const ToolCard = ({ tools, onDelete, onUpgrade, onSubmit, onCancel }: ToolCardPr
                         value: value.platform,
                         children: [
                             {
-                                label: `${value.ver}${value.review !== 'PASS' ? '*' : ''}`,
+                                label: (
+                                    <Text delete={value.review === 'REJECT'}>
+                                        {value.ver}
+                                        {value.review === 'NONE' || value.review === 'PROCESSING'
+                                            ? '*'
+                                            : ''}
+                                    </Text>
+                                ),
                                 value: value.id
                             }
                         ]
@@ -180,13 +204,22 @@ const ToolCard = ({ tools, onDelete, onUpgrade, onSubmit, onCancel }: ToolCardPr
                     tools.find((value) => value.id === selectedTool.id)!.platform,
                     selectedTool.id
                 ]}
-                displayRender={(label: string[]) => `${label[0].slice(0, 1)}-${label[1]}`}
+                displayRender={(label: ReactNode[]) =>
+                    cloneElement(label[1] as ReactElement, {
+                        children: (
+                            <>
+                                {(label[0] as string).slice(0, 1)}-
+                                {(label[1] as ReactElement).props.children}
+                            </>
+                        )
+                    })
+                }
                 onChange={handleOnVersionChange}
                 options={toolsGroupByPlatform(tools)}
             />
             {tools
                 .filter((value) => value.platform === selectedTool.platform)
-                .every((value) => value.review === 'PASS') && (
+                .every((value) => value.review === 'PASS' || value.review === 'REJECT') && (
                 <AntdTooltip title={'更新'}>
                     <Icon component={IconOxygenUpgrade} onClick={handleOnUpgradeTool} />
                 </AntdTooltip>
