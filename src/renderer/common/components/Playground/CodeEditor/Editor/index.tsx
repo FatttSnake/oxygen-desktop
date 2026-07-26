aaab5898c44aaa02b037d92b09bc5964dea40857
FatttSnake <fatttsnake@gmail.com>
2026-07-27 07:28:41 +0800

    feat(playground): support dependency version matching importMap in editor

--- src/renderer/common/components/Playground/CodeEditor/Editor/index.tsx
+++ src/renderer/common/components/Playground/CodeEditor/Editor/index.tsx
@@ -8,6 +8,7 @@ import {
     findNodeByKey,
     findNodeByPath,
     flattenFileTree,
+    getImportMap,
     getPathByKey,
     getTsconfig,
     TSCONFIG_FILE_NAME,
@@ -63,6 +64,7 @@ const Editor = ({
         language: 'none'
     }
     const selectedFilePath = getPathByKey(fileTree, selectedFileKey)
+    const importMap = useMemo(() => getImportMap(fileTree), [fileTree])
 
     const handleOnEditorWillMount = (monaco: Monaco) => {
         loadModel(monaco)
@@ -101,7 +103,7 @@ const Editor = ({
 
         onEditorDidMount?.(editor, monaco)
 
-        void autoLoadExtraLib(editor, monaco, file.content, onWatch)
+        void autoLoadExtraLib(editor, monaco, file.content, onWatch, importMap)
     }
 
     const loadModel = (monaco: Monaco) => {
